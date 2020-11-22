827a733056120f21c327c7012a84f41c6c53427d
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-22 17:43:48 +0100

    import todoListStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import TodoListStore from './redux/store/TodoListStore';
 
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">Hello World</div>
+      <TodoListStore />
     </Provider>
   );
 }
