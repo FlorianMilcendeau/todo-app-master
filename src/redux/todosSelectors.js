b13e07be9dcb90668dc2e555b9e20b5b2c452b9a
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-23 16:50:36 +0100

    delete console.log

--- src/redux/todosSelectors.js
+++ src/redux/todosSelectors.js
@@ -1,7 +1,6 @@
 export const todosSelectors = ({ todos }) => todos;
 
 export const filteredTodoSelector = ({ todos, filter }) => {
-  console.log(todos);
   if (todos.length > 0) {
     return todos.filter((todo) => filter === null || todo.completed === filter);
   }
