de76e2622d713b6190079e692ee6ce4a76408057
Florian Milcendeau <fmilcendeau85@gmail.com>
2020-11-23 14:41:26 +0100

    localStorage added

--- src/redux/TodoReducer.js
+++ src/redux/TodoReducer.js
@@ -1,17 +1,15 @@
-let id = 2;
+if (localStorage.getItem('todos') === null) {
+  localStorage.setItem('todos', []);
+}
 
-const initialState = [
-  {
-    id: 1,
-    title: 'begin the tutorial redux',
-    completed: true,
-  },
-  {
-    id: 2,
-    title: 'Finish the tutorial redux',
-    completed: false,
-  },
-];
+const storage =
+  JSON.parse(localStorage.getItem('todos')) || localStorage.getItem('todos');
+
+const lastIndex = storage.length;
+
+let id = storage.length > 0 ? storage[lastIndex - 1].id : 0;
+
+const initialState = storage;
 
 export const ADD_TODO_ACTION = 'ADD_TODO_ACTION';
 export const UPDATE_TODO_ACTION = 'UPDATE_TODO_ACTION';
@@ -19,18 +17,35 @@ export const DELETE_TODO_ACTION = 'DELETE_TODO_ACTION';
 
 export const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TODO_ACTION:
-      return [...state, { id: ++id, ...action.payload, completed: false }];
-    case UPDATE_TODO_ACTION:
-      return state.map((todo) => {
+    case ADD_TODO_ACTION: {
+      const newState = [
+        ...state,
+        { id: ++id, ...action.payload, completed: false },
+      ];
+
+      localStorage.setItem('todos', JSON.stringify(newState));
+
+      return newState;
+    }
+    case UPDATE_TODO_ACTION: {
+      const updateState = state.map((todo) => {
         if (todo.id === action.payload.id) {
           return { ...todo, ...action.payload };
         }
 
         return todo;
       });
-    case DELETE_TODO_ACTION:
-      return state.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem('todos', JSON.stringify(updateState));
+
+      return updateState;
+    }
+    case DELETE_TODO_ACTION: {
+      const newState = state.filter((todo) => todo.id !== action.payload);
+
+      localStorage.setItem('todos', JSON.stringify(newState));
+
+      return newState;
+    }
     default:
       return state;
   }
